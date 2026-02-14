99a7935870bcdce9fcec6a7caf0d847709748b72
seqrad <melrnoor772@gmail.com>
2026-02-14 10:59:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 AKI_MONGO_HOST = os.environ.get('aki_mongo_host', "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
-BOT_TOKEN = os.environ.get('bot_token', "7968843673:AAEWY9TTBYJR2-1rJmXpFWEyZ4Pkq9LCOSw")
+BOT_TOKEN = os.environ.get('bot_token', "5962156884:AAEBOd5crk8SWWQFZueWVOs2m_-Bx_UXBRM")
